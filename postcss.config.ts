2b58a7395c58e0d3e46d85ea8b953ca8164b395b
Xavier Julian <xaviju@proton.me>
2025-12-16 13:22:52 +0100

    :heavy_plus_sign: Add custom media dependency

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,7 +1,8 @@
 import postcssJitProps from 'postcss-jit-props';
 import OpenProps from 'open-props';
+import postcssCustomMedia from 'postcss-custom-media';
 
 module.exports = {
   // only vars used are in build output
-  plugins: [postcssJitProps(OpenProps)],
+  plugins: [postcssJitProps(OpenProps), postcssCustomMedia()],
 };

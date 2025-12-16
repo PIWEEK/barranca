63ad354341c6e00d0435fde8450b3306f95c8670
Xavier Julian <xaviju@proton.me>
2025-12-16 11:57:54 +0100

    :lipstick: Add hero to scroll snap

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,17 @@
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
-import './App.css'
-import HomeComponent from './sections/home/Home'
-import TimelineComponent from './sections/timeline/Timeline'
+import './App.css';
+import TimelineComponent from './sections/timeline/Timeline';
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <HomeComponent />
       <TimelineComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/sections/timeline/Timeline.tsx
+++ src/sections/timeline/Timeline.tsx
@@ -3,21 +3,25 @@ import { useState } from 'react';
 import type { TimelineEventModel } from './models/timeline.model';
 import timelineData from './data/timeline.json' assert { type: 'json' };
 
-import TimerComponent from './components/timer/timer';
-import EventComponent from './components/event/event';
+import TimerComponent from './components/Timer/timer';
+import EventComponent from './components/Event/event';
 
 import styles from './Timeline.module.css';
+import HeroComponent from './components/Hero/Hero';
 
 export default function TimelineComponent() {
   const firstDate = new Date(timelineData[0].time);
   const [currentDate, setCurrentDate] = useState(firstDate);
   return (
-    <div className="min-h-screen relative bg-slate-900 ">
-      <TimerComponent date={currentDate} />
-      <div className={styles.timeline}>
-        {timelineData.map((event: TimelineEventModel) => (
-          <EventComponent event={event} key={event.id} />
-        ))}
+    <div className={styles.timeline}>
+      <HeroComponent />
+      <div className="min-h-screen relative bg-slate-900 ">
+        <TimerComponent date={currentDate} />
+        <div>
+          {timelineData.map((event: TimelineEventModel) => (
+            <EventComponent event={event} key={event.id} />
+          ))}
+        </div>
       </div>
     </div>
   );
